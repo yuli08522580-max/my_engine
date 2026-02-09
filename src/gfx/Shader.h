92e9db15239fca4636083a32178ac386f5fbefe0
yuli08522580-max <yuli08522580@gmail.com>
2026-02-09 10:38:27 +0900

    Add descriptive comments for classes and methods

--- src/gfx/Shader.h
+++ src/gfx/Shader.h
@@ -1,27 +1,39 @@
 #pragma once
 #include <glad/glad.h>
 
+// OpenGLシェーダープログラムを管理するクラス。
 class Shader {
 public:
+    // 空のシェーダーオブジェクトを生成する。
     Shader() = default;
+    // 保持しているシェーダープログラムを解放する。
     ~Shader();
 
     Shader(const Shader&) = delete;
     Shader& operator=(const Shader&) = delete;
 
+    // プログラムハンドルの所有権をムーブする。
     Shader(Shader&& other) noexcept;
+    // プログラムハンドルの所有権をムーブ代入する。
     Shader& operator=(Shader&& other) noexcept;
 
+    // 頂点・フラグメントシェーダー文字列からプログラムを生成する。
     static Shader fromSource(const char* vsSrc, const char* fsSrc);
-    static Shader basicColor(); // いまの三角用
+    // 三角形描画用の基本色シェーダーを生成する。
+    static Shader basicColor();
 
+    // このシェーダープログラムを現在の描画状態にバインドする。
     void use() const;
+    // 内部のOpenGLプログラムIDを返す。
     GLuint id() const { return program; }
 
 private:
+    // 指定タイプのシェーダーをコンパイルする。
     static GLuint compile(GLenum type, const char* src);
+    // 頂点・フラグメントシェーダーをリンクしてプログラムを作成する。
     static GLuint link(GLuint vs, GLuint fs);
 
 private:
+    // OpenGLプログラムID。
     GLuint program = 0;
 };
--- src/scene/GameObject.h
+++ src/scene/GameObject.h
@@ -3,9 +3,13 @@
 
 class Renderer;
 
+// シーン内に配置される描画・更新可能オブジェクトの基底クラス。
 class GameObject {
 public:
+    // 派生クラス破棄のための仮想デストラクタ。
     virtual ~GameObject() = default;
+    // 1フレーム分の更新処理を行う。
     virtual void update(float dt) {}
+    // オブジェクト固有の描画処理を行う。
     virtual void render(Renderer& renderer) = 0;
 };
